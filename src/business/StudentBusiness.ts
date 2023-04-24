0626e862564f258b9bb05bed8df62c4d94c1be85
triqutran <triqutran@gmail.com>
2023-04-24 19:48:32 +0700

    Update code

--- src/business/StudentBusiness.ts
+++ src/business/StudentBusiness.ts
@@ -1,9 +1,16 @@
-interface IStudentBusiness {
+import StudentUsersDAL from "dals/StudentUsersDAL";
 
+type SignInRequest = {
+  username: string;
+  password: string;
+}
+
+interface IStudentBusiness {
+  signIn: (req: SignInRequest) => void;
 }
 
 class StudentBusiness implements IStudentBusiness {
-
+  signIn(req: SignInRequest): void {}
 }
 
 export default new StudentBusiness();
